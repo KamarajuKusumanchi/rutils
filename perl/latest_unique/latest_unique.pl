008e2acf7b1e3376bcc9382764ea3bc68279470f
Kamaraju Kusumanchi <kamaraju@gmail.com>
2014-07-01 00:32:37 -0400

    added latest_uniq.pl

---
+++ perl/latest_unique/latest_unique.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/perl
+
+# print unique lines in a file by preserving the order and retaining the latest
+# of duplicated lines.
+
+use List::MoreUtils qw(uniq);	# uniq guarantees the original order
+
+@lines = <>;
+@latest_unique = reverse uniq reverse @lines;
+print @latest_unique;
+
+
+# sample usage
+# rajulocal@hogwarts:~/work/rutils/perl/latest_unique$ ./latest_unique.pl input.txt 
+# ls -al
+# top
+# cd /usr/bin
+# whoami
+# ls -rt
+# rajulocal@hogwarts:~/work/rutils/perl/latest_unique$ cat input.txt 
+# whoami
+# ls -al
+# top
+# whoami
+# ls -rt
+# cd /usr/bin
+# whoami
+# ls -rt
