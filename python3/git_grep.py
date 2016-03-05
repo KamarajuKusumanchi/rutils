d8bf45950d5d9c90c91309e2b456348d3358cb5b
Kamaraju S. Kusumanchi <kamaraju@gmail.com>
2016-03-04 23:57:33 -0500

    Initial commit of git_grep.py

---
+++ python3/git_grep.py
@@ -0,0 +1,54 @@
+#! /usr/bin/env python3
+
+import subprocess
+import argparse
+import os
+
+def do_it(cmd, debug):
+    if (debug):
+        print(cmd)
+    subprocess.call([cmd], shell=True)
+
+def get_gitroot(dirname):
+    ''' get the top level git directory.
+    If it is not found, return the argument itself.
+
+    Ref:- https://github.com/MaxNoe/python-gitpath/
+    '''
+    try:
+        os.chdir(dirname)
+        base = subprocess.check_output(['git', 'rev-parse', '--show-toplevel'])
+    except:
+        print("No git repository found for ", dirname);
+        return dirname
+    return base.decode('utf-8').strip()
+
+
+def do_grep(args):
+    pattern = args.pattern
+    # conversion to absolute path becomes important if something like ../.. is
+    # passed.
+    directory = get_gitroot( os.path.abspath(args.dir) )
+    
+    # enclose the pattern in double quotes.
+    cmd = 'grep -sir "' + pattern + '" ' + directory +  \
+    ' --exclude-dir=".git" --color'
+    do_it(cmd, args.debug)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description='grep a git repository',
+        )
+    parser.add_argument("pattern", action="store",
+        help="pattern to search for"
+        )
+    parser.add_argument("dir", nargs='?',
+        default=os.getcwd(),
+        help="directory"
+        )
+    parser.add_argument("--debug", action="store_true",
+        default=False, dest="debug",
+        help="show debug output"
+        )
+    args = parser.parse_args()
+    do_grep(args)
