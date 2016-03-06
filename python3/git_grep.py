8f37397b21bc06364160c53e43d1167a40ab1961
Kamaraju S. Kusumanchi <kamaraju@gmail.com>
2016-03-06 10:29:46 -0500

    Make git_grep.py pep8 compliant.

--- python3/git_grep.py
+++ python3/git_grep.py
@@ -4,7 +4,7 @@ import subprocess
 import argparse
 import os
 
-''' A handy utility to grep a git repository while excluding the .git directory.
+''' Utility program to grep a git repository but exclude the .git directory.
 
 Call it as
   <scriptname> pattern [directory]
@@ -15,10 +15,12 @@ repository will be used in searching. Otherwise an error message is printed and
 grepping is done in the original directory.
 '''
 
+
 def do_it(cmd, debug):
     if (debug):
         print(cmd)
     subprocess.call([cmd], shell=True)
+
 
 def get_gitroot(dirname):
     ''' get the top level git directory.
@@ -30,7 +32,7 @@ def get_gitroot(dirname):
         os.chdir(dirname)
         base = subprocess.check_output(['git', 'rev-parse', '--show-toplevel'])
     except:
-        print("No git repository found for ", dirname);
+        print("No git repository found for ", dirname)
         return dirname
     return base.decode('utf-8').strip()
 
@@ -39,11 +41,11 @@ def do_grep(args):
     pattern = args.pattern
     # conversion to absolute path seems to be important if something like ../..
     # is passed.
-    directory = get_gitroot( os.path.abspath(args.dir) )
-    
+    directory = get_gitroot(os.path.abspath(args.dir))
+
     # enclose the pattern in double quotes.
     cmd = 'grep -sir "' + pattern + '" ' + directory +  \
-    ' --exclude-dir=".git" --color'
+        ' --exclude-dir=".git" --color'
     do_it(cmd, args.debug)
 
 if __name__ == "__main__":
@@ -51,13 +53,13 @@ if __name__ == "__main__":
         description='grep a git repository',
         )
     parser.add_argument("pattern", action="store",
-        help="pattern to search for")
+                        help="pattern to search for")
     parser.add_argument("dir", nargs='?',
-        default=os.getcwd(),
-        help="directory")
+                        default=os.getcwd(),
+                        help="directory")
     parser.add_argument("--debug", action="store_true",
-        default=False, dest="debug",
-        help="show debug output")
+                        default=False, dest="debug",
+                        help="show debug output")
     args = parser.parse_args()
 
     do_grep(args)
