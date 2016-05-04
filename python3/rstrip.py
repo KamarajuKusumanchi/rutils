ec44d29fe44317419eeb1b0ad87c8731fd9f7087
d3k2mk7 <kamaraju@gmail.com>
2016-05-04 11:04:00 -0400

    Read either from a file or from stdin

--- python3/rstrip.py
+++ python3/rstrip.py
@@ -1,29 +1,44 @@
 #! /usr/bin/env python3
 
-""" Remove trailing whitespaces from each line of a file. By default, the
-output will be sent to stdout.
+""" Remove trailing whitespaces from each line. The input is read from a file
+or from stdin if the file name is '-'. The output is sent to stdout.
 
 Sample usage:
-% python3 ~/work/gitlab/rutils/python3/rstrip.py ~/x/isp.txt | rev | cut -f 1 -d ',' | rev | sort | uniq -c | sort -n | wc -l
+ % python3 ./rstrip.py ~/x/junk73.txt > ~/x/junk74.txt
+ % cat ~/x/junk73.txt | python3 ./rstrip.py - > ~/x/junk75.txt
+ 
+ % python3 ./rstrip.py ~/x/isp.txt | rev | cut -f 1 -d ',' | rev | sort | uniq -c | sort -n | wc -l
 46
 """
 
+import sys
+
 def strip_trailing_whitespace(args):
     fname = args.fname
-    fh = open(fname)
+    using_stdin = (fname == "-")
+
+    if (not using_stdin):
+        fh = open(fname)
+    else:
+        fh = sys.stdin
+
     for line in fh:
         line = line.rstrip()
         print(line)
+
+    if (not using_stdin):
+        fh.close()
 
 if __name__ == "__main__":
 
     import argparse
     parser = argparse.ArgumentParser(
-        description='''Remove trailing whitespaces from each line of a file.
-        By default, the output will be send to stdout''')
+        description='''Remove trailing whitespaces from each line. The input
+        is read from a file or from stdin if the file name is '-'. The output
+        is send to stdout''')
     parser.add_argument(
         "fname", action="store",
-        help = "Input file name")
+        help = "file to read from. Use '-' to read from stdin")
     args = parser.parse_args()
 
     strip_trailing_whitespace(args)
