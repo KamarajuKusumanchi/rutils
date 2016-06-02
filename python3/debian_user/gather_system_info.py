75e8516cbb8fee44b5de6a68b8479e3067f07968
Kamaraju S. Kusumanchi <kamaraju@gmail.com>
2016-06-02 01:37:14 -0400

    gather system information for sound task

--- python3/debian_user/gather_system_info.py
+++ python3/debian_user/gather_system_info.py
@@ -1,7 +1,56 @@
 import re
+import subprocess
 
-# Todo: compact_print function already exists in ../stuff.py
-# use it instead of copying it here.
+''' This script aims to gather system information for common configuration
+tasks such as sound, graphics, wireless, network, printer etc., Initially, only
+the sound task is supported. Support for other tasks will be added in the due
+course.
+
+Output:
+Relevant system information will be printed to stdout.
+
+Potential use case:
+When requesting help on a particular task on debian-user mailing list, the
+requestor can simply copy paste the output of this script into the initial
+email so that experts will have all the necessary information to solve the
+issue at hand.
+
+Todo:
+Move functions such as compact_print into a standalone
+module instead of copying them everywhere. For example these functions
+already exist in ../stuff.py
+'''
+
+
+def sound():
+    run_lspci()
+    run_lsmod()
+    print_sources_list()
+    kernel_version()
+
+
+def run_lspci():
+    cmd = "lspci -nnk | grep -i audio"
+    print(cmd)
+    output = subprocess.check_output(cmd, shell=True, universal_newlines=True)
+    print(output)
+
+
+def run_lsmod():
+    cmd = "lsmod | grep snd"
+    print(cmd)
+    output = subprocess.check_output(cmd, shell=True, universal_newlines=True)
+    print(output)
+
+
+def print_sources_list():
+    fname = "/etc/apt/sources.list"
+    comment_pattern = "^\s*#"
+    print("\n# contents of ", fname)
+    compact_print(fname, comment_pattern)
+    print()
+
+
 def compact_print(fname, comment_pattern):
     empty_pattern = "^\s*$"
 
@@ -15,13 +64,13 @@ def compact_print(fname, comment_pattern):
         else:
             print(line)
 
-def print_sources_list():
-    fname = "/etc/apt/sources.list"
-    comment_pattern = "^\s*#"
-    # print("-"*80)
-    print("\ncontents of ", fname, "\n")
-    compact_print(fname, comment_pattern)
-    # print("-"*80)
+
+def kernel_version():
+    cmd = "uname -a"
+    print(cmd)
+    output = subprocess.check_output(cmd, shell=True, universal_newlines=True)
+    print(output)
+
 
 if __name__ == "__main__":
-    print_sources_list()
+    sound()
