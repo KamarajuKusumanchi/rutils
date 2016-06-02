a21675e34671e0bcc7df48ef8b88aa5aea8ae36c
Kamaraju S. Kusumanchi <kamaraju@gmail.com>
2016-06-02 01:34:45 -0400

    add debug option to the do_it function

--- python3/echo_and_execute.py
+++ python3/echo_and_execute.py
@@ -1,15 +1,16 @@
 import subprocess
 
 
-def do_it(cmd):
-  print(cmd)
-  subprocess.call([cmd], shell=True)
+def do_it(cmd, debug=1):
+    if (debug):
+        print(cmd)
+    subprocess.call([cmd], shell=True)
 
 def check_output(cmd):
     cmdstr = " ".join(cmd)
     print(cmdstr)
     out = subprocess.check_output(cmd, universal_newlines=True)
-    print(out)
+    print(out, end='')
     return(cmdstr, out)
 
 do_it("ls -al")
