a37c481256473f2b82cb7f3b267850acb2172fed
Kamaraju S. Kusumanchi <kamaraju@gmail.com>
2016-06-21 23:19:53 -0400

    Add git up

--- bin/git_commit.sh
+++ bin/git_commit.sh
@@ -7,7 +7,11 @@ do_it()
   echo ---------------------------------------------
   set -x
   cd ~/work/gitlab/rutils
-  git commit -m "Initial commit of bin/git-up"
+  git commit -m "Initial commit of bin/popsort.py"
+
+  # This calls git-up which is a custom script.
+  # It is part of this package.
+  git up
   git push -u origin master
 }
 
