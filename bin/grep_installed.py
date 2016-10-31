9ebfb9667da652bb773d7c6a3a1b1ab35b2c9e4a
Kamaraju S. Kusumanchi <kamaraju@gmail.com>
2016-10-30 21:36:18 -0400

    Factor out the code that reads the txt.gz file

--- bin/grep_installed.py
+++ bin/grep_installed.py
@@ -45,22 +45,11 @@ def add_distribution_columns(df, args):
         return
 
     for distribution in all_dists:
-        fname = '/home/rajulocal/x/' + distribution + '.txt.gz'
-
-        try:
-            # Check if the file can be opened.
-            with gzip.open(fname, 'rt') as f:
-                for i in range(6):
-                    ignore = f.readline()
-                dist_packages = [line.split(' ', 1)[0] for line in f]
-            df[distribution] = [
-                True if pkg in dist_packages else False
-                for pkg in df['pkg']
-            ]
-        except:
-            print("Unable to open", fname, "while processing", distribution,
-                  "distribution")
+        dist_packages = read_compact_compressed_allpackages_list(distribution)
+        if (dist_packages.empty):
             continue
+
+        df[distribution] = df['pkg'].isin(dist_packages['pkg'])
 
 
 def get_all_dists(args):
@@ -87,6 +76,31 @@ def get_dists_to_exclude(args):
     if (exclude_dist is not None):
         dists_to_exclude = [x.strip() for x in exclude_dist.split(',')]
     return dists_to_exclude
+
+
+# For a given distribution, a list of all available packages on all
+# architectures can be downloaded from
+# https://packages.debian.org/${distribution}/allpackages?format=txt.gz . Here
+# the ${distribution} can be an actual name such as jessie, stretch, sid etc.,
+# or a symbolic name such as stable, testing, unstable etc., For example, for
+# sid this would be https://packages.debian.org/sid/allpackages?format=txt.gz .
+#
+# The idea here is to process this file and return a data frame containing the
+# list of packages.
+def read_compact_compressed_allpackages_list(distribution):
+    fname = '/home/rajulocal/x/' + distribution + '.txt.gz'
+
+    df = pd.DataFrame(None)
+    try:
+        # Check if the file can be opened.
+        with gzip.open(fname, 'rt') as f:
+            for i in range(6):
+                ignore = f.readline()
+            df['pkg'] = [line.split(' ', 1)[0] for line in f]
+    except:
+        print("Unable to open", fname, "while processing", distribution,
+              "distribution")
+    return df
 
 
 def show_installed(args):
@@ -176,6 +190,11 @@ def parse_arguments(args):
         help='''Show only if the package is not from this distribution. It can be a
         comma separated list if mutliple distributions are involved.
        ''')
+    # parser.add_argument(
+    #     "--update", action="store_true",
+    #     dest="update_cache",
+    #     help='''update the package lists'''
+    #     )
     parser.add_argument(
         "--debug", action='store_true',
         default=False, dest='debug',
@@ -183,7 +202,10 @@ def parse_arguments(args):
     return parser.parse_args()
 
 
-if __name__ == "__main__":
+def run_code():
     args = parse_arguments(sys.argv[1:])
     show_installed(args)
     # show_installed_simple()
+
+if __name__ == "__main__":
+    run_code()
