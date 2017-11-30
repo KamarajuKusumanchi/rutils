8e11f244885291677cf6c68330ce1beae35bf5a6
Kamaraju S. Kusumanchi <kamaraju@gmail.com>
2017-11-29 22:16:29 -0800

    Show a directory only if it has any changes.

--- bin/git_status.sh
+++ bin/git_status.sh
@@ -11,10 +11,25 @@ do
     for project in `ls $i`
     do
         abs_path=$i/$project
-        echo $abs_path
 
-	# The -C is supported in git >= 1.8.5
+        # In the git status command below:
 	# -s gives the output in short-format
-        git -C $abs_path status -s
+        # -b shows the branch and tracking information
+	# -C is used to specify the project directory.
+        # The -C option is supported in git >= 1.8.5
+        #
+	# I am not interested if the output of "git status -sb" is just
+        # ## master...origin/master
+        # But want to capture things like
+        # ## master...origin/master [ahead 1]
+        # to show if the repository is ahead of 'origin/master'.
+        output=$(git -C $abs_path status -sb | grep -v "^## master\.\.\.origin/master$")
+
+	# Simply printing $output will not show any colors. So rerun the git
+	# command, which will give colored output.
+        if [ -n "$output" ]; then
+            echo $abs_path
+            git -C $abs_path status -sb
+        fi
     done
 done
