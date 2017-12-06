9a882c4bd380326669f1e5b805460c4a4594c10a
Kamaraju S. Kusumanchi <kamaraju@gmail.com>
2017-12-06 22:12:00 +0000

    Convert tabs to spaces

--- bin/git_status.sh
+++ bin/git_status.sh
@@ -13,20 +13,20 @@ do
         abs_path=$i/$project
 
         # In the git status command below:
-	# -s gives the output in short-format
+        # -s gives the output in short-format
         # -b shows the branch and tracking information
-	# -C is used to specify the project directory.
+        # -C is used to specify the project directory.
         # The -C option is supported in git >= 1.8.5
         #
-	# I am not interested if the output of "git status -sb" is just
+        # I am not interested if the output of "git status -sb" is just
         # ## master...origin/master
         # But want to capture things like
         # ## master...origin/master [ahead 1]
         # to show if the repository is ahead of 'origin/master'.
         output=$(git -C $abs_path status -sb | grep -v "^## master\.\.\.origin/master$")
 
-	# Simply printing $output will not show any colors. So rerun the git
-	# command, which will give colored output.
+        # Simply printing $output will not show any colors. So rerun the git
+        # command, which will give colored output.
         if [ -n "$output" ]; then
             echo $abs_path
             git -C $abs_path status -sb
