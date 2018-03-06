7ad464a9a48f650895eab370dd155aaf122b94f0
Kamaraju S. Kusumanchi <kamaraju@gmail.com>
2018-03-06 18:08:34 -0500

    Initial commit of run_as_cron.sh

---
+++ bin/run_as_cron.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/env bash
+
+# To generate the cron environment, add this to crontab and wait for a minute
+#* * * * *   /usr/bin/env > $HOME/x/cron-env
+# once the file is populated, edit crontab and disable that line.
+#
+# To use this script
+# run_as_cron.sh /the/problematic/script --with arguments --and parameters
+/usr/bin/env -i $(cat $HOME/x/cron-env) "$@"
