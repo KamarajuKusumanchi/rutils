bf9d0f6b3ce4d9350c45fe9be8724aa3c3f2bbf9
Kamaraju S. Kusumanchi <kamaraju@gmail.com>
2018-03-12 16:11:38 -0400

    Improved formatting of comments

--- bin/run_as_cron.sh
+++ bin/run_as_cron.sh
@@ -3,7 +3,7 @@
 # Run any script in an emulated cron environment.
 #
 # To generate the cron environment, add this to crontab and wait for a minute
-#* * * * *   /usr/bin/env > $HOME/x/cron-env
+# * * * * *   /usr/bin/env > $HOME/x/cron-env
 # once the file is populated, edit crontab and disable that line.
 #
 # To use this script
