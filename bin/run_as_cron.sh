74b5f7904697a488dba7261fc4984cc4635f84b2
Kamaraju S. Kusumanchi <kamaraju@gmail.com>
2018-03-12 16:08:18 -0400

    Improved description

--- bin/run_as_cron.sh
+++ bin/run_as_cron.sh
@@ -1,5 +1,7 @@
 #! /usr/bin/env bash
 
+# Script to test something that is to be run under cron.
+#
 # To generate the cron environment, add this to crontab and wait for a minute
 #* * * * *   /usr/bin/env > $HOME/x/cron-env
 # once the file is populated, edit crontab and disable that line.
