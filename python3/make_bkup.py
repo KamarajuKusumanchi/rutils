03aa9a3e01eb4586b71c771de7c43924af5b302d
Kamaraju S. Kusumanchi <kamaraju@gmail.com>
2018-07-17 23:47:58 -0400

    Added comments

--- python3/make_bkup.py
+++ python3/make_bkup.py
@@ -1,5 +1,10 @@
 #! /usr/bin/env python3
 
+# Script to backup a file or directory
+#
+# The input is copied to src_asof_YYYYMMDD_HHmmSS where src is the original
+# file or directory and YYYYMMDD_HHmmSS is its last modified time.
+
 import sys
 from lib.file_utils import backup_with_timestamp
 
