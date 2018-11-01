fa6ccdc4cb37c8c55ed5128e2ccc940564330f16
Kamaraju S. Kusumanchi <kamaraju@gmail.com>
2018-11-01 18:16:52 -0400

    utility to function to get the size of a directory

---
+++ python3/disk_usage.py
@@ -0,0 +1,21 @@
+import os
+
+def get_size(path):
+    '''
+    Get size of a file or folder.
+    Should give the same output as "du -sb <path>"
+    :param path: The absolute path of a file or directory.
+    :return:
+    '''
+    size = 0
+
+    # If path is a file, os.walk(path) for loop  will not even run once.
+    # So take care of this separately.
+    if os.path.isfile(path):
+        return os.path.getsize(path)
+
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            file_path = os.path.join(root, file)
+            size += os.path.getsize(file_path)
+    return size
