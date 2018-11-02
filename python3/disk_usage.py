8ce88a6544b37254d8f7ed91403f6f439f319c53
Kamaraju S. Kusumanchi <kamaraju@gmail.com>
2018-11-02 12:02:42 -0400

    Add main function

--- python3/disk_usage.py
+++ python3/disk_usage.py
@@ -1,4 +1,6 @@
+import argparse
 import os
+import sys
 
 def get_size(path):
     '''
@@ -19,3 +21,20 @@ def get_size(path):
             file_path = os.path.join(root, file)
             size += os.path.getsize(file_path)
     return size
+
+def parse_arguments(args):
+    parser = argparse.ArgumentParser(
+        description='Get disk usage.'
+    )
+    parser.add_argument(
+        'file', action='store',
+        help='disk usage of a file or a directory'
+    )
+    args = parser.parse_args()
+    return args
+
+if __name__ == '__main__':
+    args = parse_arguments(sys.argv[1:])
+    path = os.path.abspath(args.file)
+    size = get_size(path)
+    print(size, '\t', path)
