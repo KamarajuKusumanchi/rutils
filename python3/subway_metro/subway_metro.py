2aea9a9c85289ab84dc3eb2295dbe3e8c476cd75
Kamaraju S. Kusumanchi <kamaraju@gmail.com>
2018-11-05 01:06:00 -0500

    Codestyle fixes

--- python3/subway_metro/subway_metro.py
+++ python3/subway_metro/subway_metro.py
@@ -11,5 +11,5 @@ to_purchase = 0
 print(remaining_balance)
 while (remaining_balance > 0.01):
     to_purchase += 1
-    remaining_balance = (initial_balance + to_purchase*(1+bonus))  % ticket_price
-    print("to_purchase = ", to_purchase, ", remaining_balance = %.2f" %remaining_balance)
+    remaining_balance = (initial_balance + to_purchase*(1+bonus)) % ticket_price
+    print("to_purchase = ", to_purchase, ", remaining_balance = %.2f" % remaining_balance)
