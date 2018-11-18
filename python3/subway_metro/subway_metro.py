11cbad1e78999b8562f9aa421a3cd308064e1970
Kamaraju S. Kusumanchi <kamaraju@gmail.com>
2018-11-18 17:40:45 -0500

    Update code

--- python3/subway_metro/subway_metro.py
+++ python3/subway_metro/subway_metro.py
@@ -3,13 +3,19 @@ import sys
 ticket_price = 2.75
 bonus = 0.11
 min_purchase = 10
+remaining_balance_threshold = 0.01
+
+# ticket_price = 1.75
+# bonus = 0
+# remaining_balance_threshold = 0.05
+
 initial_balance = float(sys.argv[1])
 
 remaining_balance = initial_balance % ticket_price
 
 to_purchase = 0
 print(remaining_balance)
-while (remaining_balance > 0.01):
+while (remaining_balance > remaining_balance_threshold):
     to_purchase += 1
     remaining_balance = (initial_balance + to_purchase*(1+bonus)) % ticket_price
     print("to_purchase = ", to_purchase, ", remaining_balance = %.2f" % remaining_balance)
