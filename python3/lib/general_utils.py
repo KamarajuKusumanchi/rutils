edf96be9766b33475ff70673246c27b2a47fb222
Kamaraju S. Kusumanchi <kamaraju@gmail.com>
2019-01-04 17:57:10 -0500

    Add stuff

---
+++ python3/lib/general_utils.py
@@ -0,0 +1,21 @@
+import psutil
+
+
+def worker_count(keep_free=1):
+    """
+    The idea here is to get the number of worker processes that can be
+    started while keeping some resources free. The total number of
+    resources is assumed to be the logical cpu count (physical cores *
+    number of hyper threads)
+    :param keep_free: Number of resources to be kept free
+    :return: workers to use
+    """
+    available = psutil.cpu_count()
+    if available > keep_free:
+        available -= keep_free
+    return available
+
+
+if __name__ == '__main__':
+    workers_available = worker_count()
+    print('workers available = ', workers_available)
