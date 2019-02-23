03372e248c1f9f853c8f67cdcf60d56458db62fe
Kamaraju S. Kusumanchi <kamaraju@gmail.com>
2019-02-22 20:31:16 -0500

    lgtm

--- python3/break_into_chunks.py
+++ python3/break_into_chunks.py
@@ -3,26 +3,38 @@
 
 import pandas as pd
 
-def chunkify(N, p):
+def partition_tasks(N, p):
+    '''
+    The idea here is to uniformly partition N tasks over p workers
+    by breaking a hypothetical N element array into p contiguous chunks.
+    When N is divisible by p, each chunk will have equal number of tasks.
+    When N is not divisible by p, the first N modulo(p) tasks have one
+    extra task.
+
+    It returns a Series with p+1 elements corresponding to the
+    partition boundaries.
+
+    Example: For N=13, p=4 the return Series is [0,4,7,10,13] which means
+    the partitions are [0:4], [4:7], [7:10], [10:13]
+    :param N:
+    :param p:
+    :return:
+    '''
     # assume N >0, p>0
     quotient = int(N/p)
     remainder = N % p
     nelem = pd.Series([quotient]* p)
     nelem[0:remainder] += 1
-    indexList = pd.Series([0]).append(nelem.cumsum())
+    partitionIndex = pd.Series([0]).append(nelem.cumsum(), ignore_index=True)
+    return partitionIndex
 
-    res = pd.DataFrame({'indexLow': indexList[0:p].values,
-                        'indexHigh': indexList[1:p+1].values})
-    res['nelem'] = nelem
-    return res
-
-a = chunkify(12, 4)
+a = partition_tasks(12, 4)
 print(a)
-a = chunkify(13, 4)
+a = partition_tasks(13, 4)
 print(a)
-a = chunkify(14, 4)
+a = partition_tasks(14, 4)
 print(a)
-a = chunkify(15, 4)
+a = partition_tasks(15, 4)
 print(a)
-a = chunkify(16, 4)
+a = partition_tasks(16, 4)
 print(a)
