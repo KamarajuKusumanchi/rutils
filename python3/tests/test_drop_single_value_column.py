090c73fbb57468c4c9d576f20d4a44f5fd81c454
Kamaraju S. Kusumanchi <kamaraju@gmail.com>
2020-04-06 13:34:40 -0400

    lgtm

---
+++ python3/tests/test_drop_single_value_column.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+from drop_single_value_columns import drop_single_value_columns
+from pandas.testing import assert_frame_equal
+
+
+def test_drop_single_value_columns():
+    df = pd.DataFrame({'A': [None, None],
+                       'B': [None, 1],
+                       'C': [2, 3],
+                       'D': [4, 4]})
+    df_got = drop_single_value_columns(df)
+    df_expected = pd.DataFrame({'B': [None, 1],
+                           'C': [2, 3]})
+    assert_frame_equal(df_got, df_expected)
