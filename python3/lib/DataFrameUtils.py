0a752ff4550fe8c2bb146c6a6083d68411b35851
Kamaraju S. Kusumanchi <kamaraju@gmail.com>
2023-02-01 11:07:51 -0500

    lgtm

--- python3/lib/DataFrameUtils.py
+++ python3/lib/DataFrameUtils.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 
 def print_without_index(df: pd.DataFrame):
+    # print dataframe without index
     # Got the idea from https://stackoverflow.com/questions/24644656/how-to-print-pandas-dataframe-without-index
     blank_index = [""] * len(df)
     original_index = df.index
