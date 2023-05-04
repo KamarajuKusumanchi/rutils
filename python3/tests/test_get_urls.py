30f74183bba78c50735ee9b42ab9145dcf0a487a
Kamaraju S. Kusumanchi <kamaraju@gmail.com>
2023-05-03 23:45:08 -0400

    fix unit tests

--- python3/tests/test_get_urls.py
+++ python3/tests/test_get_urls.py
@@ -1,3 +1,5 @@
+import os
+
 from get_urls import get_urls
 
 
@@ -14,7 +16,9 @@ def test_get_urls_count():
 def test_get_urls_text():
     url = "https://news.ycombinator.com/item?id=25271676"
     urls_got = get_urls(url)
-    fname = "data/urls_hn_25271676.txt"
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    data_dir = os.path.join(dir_path, "data")
+    fname = os.path.join(data_dir, "urls_hn_25271676.txt")
     with open(fname, "r") as fh:
         urls_expected = [line.rstrip() for line in fh]
     assert urls_got == urls_expected, "urls do not match"
