7b19a5d6d72a4e79144036cf516bf1ce09bd10f7
Kamaraju S. Kusumanchi <kamaraju@gmail.com>
2024-04-16 08:12:09 -0400

    lgtm

--- python3/src/scripts/chessli_to_anki.py
+++ python3/src/scripts/chessli_to_anki.py
@@ -2,7 +2,8 @@
 import pandas as pd
 import sys
 df = pd.read_csv(sys.stdin, dtype='str')
-mask = (df['nag_name'] == 'blunder') | (df['nag_name'] == 'mistake')
+# mask = (df['nag_name'] == 'blunder') | (df['nag_name'] == 'mistake')
+mask = (df['nag_name'] == 'blunder')
 df = df[mask]
 df[['variation', 'best_move']] = df[['best_move', 'variation']]
 df.to_csv(sys.stdout, index=False, header=False)
