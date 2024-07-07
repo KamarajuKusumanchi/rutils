10c75c13d92a5c3b68c0970ad8087683c39490c6
Kamaraju S. Kusumanchi <kamaraju@gmail.com>
2024-07-07 02:31:43 -0400

    lgtm

--- python3/src/books/book_sort.py
+++ python3/src/books/book_sort.py
@@ -10,9 +10,11 @@ if __name__ == "__main__":
     ratings_file_path = os.path.join(data_dir, ratings_file_name)
     ratings = pd.read_csv(ratings_file_path, comment='#')
     # print(ratings)
+    # print()
     book_list = sys.stdin.read().splitlines()
     book_df = pd.DataFrame({'book': book_list})
     book_df = book_df.merge(ratings, on=['book'], how='left')
     book_df['rating'].fillna(0, inplace=True)
     book_df.sort_values(by=['rating'], ascending=False, inplace=True)
+    # book_df.to_csv(sys.stdout, index=False)
     [print(x) for x in book_df['book'].values]
