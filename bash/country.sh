0c810654a015426b28ba13a6c5eec644103a621d
Kamaraju S. Kusumanchi <kamaraju@gmail.com>
2026-02-10 20:29:25 +0000

    lgtm

--- bash/country.sh
+++ bash/country.sh
@@ -9,9 +9,23 @@
 # 37.238.101.42, IQ, Iraq
 # ...
 
-# Since we are writing a script, it is better to use while loop than xargs
-# Read IP addresses from stdin, sort them, and get their location
-sort | awk '{print $1}' | uniq | while read -r ip; do
+# Since we are writing a script, it is better to
+# - use while loop instead of xargs
+# - let upstream handle transforming the inputs
+#
+# expected input:
+# - one ip address per line
+#
+# sample command:
+# grep "^37.238" /var/log/nginx/access.log | awk '{print $1}' | sort | uniq | <script_name.sh>
+#
+# sample output:
+# 37.238.100.8, IQ, Iraq
+# 37.238.101.164, IQ, Iraq
+# 37.238.101.42, IQ, Iraq
+#
+# Read IP addresses from stdin and get their location
+while read -r ip; do
     # Get GeoIP info and format output
     location=$(geoiplookup "$ip" | awk -F": " '{print $2}')
     echo "$ip, $location"
