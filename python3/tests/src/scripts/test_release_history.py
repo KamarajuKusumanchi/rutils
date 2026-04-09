d637ca368c9c410fef6e609d8874a07192fb14f9
Kamaraju S. Kusumanchi <kamaraju@gmail.com>
2026-04-09 11:49:01 -0400

    lgtm

--- python3/tests/src/scripts/test_release_history.py
+++ python3/tests/src/scripts/test_release_history.py
@@ -5,10 +5,20 @@ from src.scripts.release_history import python_release_history
 
 
 def test_python_release_history():
-    data = {'version': ["3.14.0", "3.14.1", "3.14.2", "3.14.3", "3.14.4"],
-            'release_date': ["2025-10-07", "2025-12-02", "2025-12-05", "2026-02-03", "2026-04-07"]}
+    data = {
+        'version': ["3.14.0", "3.14.1", "3.14.2", "3.14.3", "3.14.4"],
+        'release_date': [
+            "2025-10-07",
+            "2025-12-02",
+            "2025-12-05",
+            "2026-02-03",
+            "2026-04-07",
+        ],
+    }
 
     df_expected = pd.DataFrame(data)
-    df_expected['release_date'] = pd.to_datetime(df_expected['release_date'], errors="coerce").dt.date
+    df_expected['release_date'] = pd.to_datetime(
+        df_expected['release_date'], errors="coerce"
+    ).dt.date
     df_got = python_release_history(limit=5)
     assert_frame_equal(df_got, df_expected)
