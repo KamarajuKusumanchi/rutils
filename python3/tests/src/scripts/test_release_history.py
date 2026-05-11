aa565f458cbff7d56c98fa2efb2d64b22743776e
Kamaraju S. Kusumanchi <kamaraju@gmail.com>
2026-05-11 17:15:55 -0400

    lgtm

--- python3/tests/src/scripts/test_release_history.py
+++ python3/tests/src/scripts/test_release_history.py
@@ -6,13 +6,13 @@ from src.scripts.release_history import python_release_history
 
 def test_python_release_history():
     data = {
-        'version': ["3.14.0", "3.14.1", "3.14.2", "3.14.3", "3.14.4"],
+        'version': ["3.14.1", "3.14.2", "3.14.3", "3.14.4", "3.14.5"],
         'release_date': [
-            "2025-10-07",
             "2025-12-02",
             "2025-12-05",
             "2026-02-03",
             "2026-04-07",
+            "2026-05-10",
         ],
     }
 
